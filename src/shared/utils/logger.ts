a858b55c170ec2dc96011a08ce3f6e906a8f7334
dimas <dimsmukhtar@gmail.com>
2025-04-19 19:38:10 +0700

    fixing logger

--- src/shared/utils/logger.ts
+++ src/shared/utils/logger.ts
@@ -2,6 +2,11 @@ import winston from 'winston'
 
 export const logger = winston.createLogger({
   level: 'debug',
-  format: winston.format.json(),
-  transports: [new winston.transports.Console({})]
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.printf(({ level, message }) => {
+      return `${new Date().toISOString()} ${level}: ${message}`
+    })
+  ),
+  transports: [new winston.transports.Console()]
 })
