88ce9920fd4efa1b99652929534f3037ccc0fb8d
dimas <dimsmukhtar@gmail.com>
2025-07-16 16:35:27 +0700

    add random code generator to ticket code

--- src/applications/modules/Transaction/webhook.controller.ts
+++ src/applications/modules/Transaction/webhook.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import { snap } from '../../../shared/utils/midtrans'
 import { prisma } from '../../../infrastructure/database/client'
 import { BookingStatus, SeatStatus, TicketStatus } from '@prisma/client'
+import { generateRandomCode } from '../../../shared/helpers/randomCode'
 
 export const midtransWebhookHandler = async (req: Request, res: Response) => {
   try {
@@ -49,11 +50,14 @@ export const midtransWebhookHandler = async (req: Request, res: Response) => {
             }
           })
 
-          const ticketData = transaction.transaction_items.map((item) => ({
-            code: `TICKET-${transaction.id}-${item.id}`,
-            status: TicketStatus.active,
-            transaction_item_id: item.id
-          }))
+          const ticketData = transaction.transaction_items.map((item) => {
+            const randomCode = generateRandomCode(5)
+            return {
+              code: `TICKET-${transaction.id}-${item.id}-${randomCode}`,
+              status: TicketStatus.active,
+              transaction_item_id: item.id
+            }
+          })
 
           await tx.ticket.createMany({
             data: ticketData
